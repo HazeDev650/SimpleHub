9189f579bfeaa978ead7c130540229e51cf5e1f9
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-09-18 21:46:05 -0700

    Update DeleteHubCommand.php

--- src/Terpz710/SimpleHub/command/DeleteHubCommand.php
+++ src/Terpz710/SimpleHub/command/DeleteHubCommand.php
@@ -9,10 +9,13 @@ use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat;
 use pocketmine\player\Player;
 use Terpz710\SimpleHub\Main;
+use pocketmine\plugin\PluginOwned;
+use pocketmine\plugin\PluginOwnedTrait;
 
-class DeleteHubCommand extends Command {
+class DeleteHubCommand extends Command implements PluginOwned {
+    use PluginOwnedTrait;
 
-    public function __construct() {
+    public function __construct(Main $plugin) {
         parent::__construct(
             "deletehub",
             "Delete the hub location",
@@ -20,6 +23,7 @@ class DeleteHubCommand extends Command {
             ["removehub"]
         );
         $this->setPermission("deletehub.command");
+        $this->setOwningPlugin($plugin);
     }
 
     public function execute(CommandSender $sender, string $label, array $args) {
