23343d10618c1fae898a910ece0b957c29243ec5
HazeDev650 <145964153+HazeDev650@users.noreply.github.com>
2023-09-25 01:04:51 -0700

    Update Main.php

--- src/Terpz710/SimpleHub/Main.php
+++ src/Terpz710/SimpleHub/Main.php
@@ -16,8 +16,12 @@ class Main extends PluginBase implements Listener {
 
     /** @var WorldManager */
     private $worldManager;
+    
+    // Store hub locations for each world
+    private $hubLocations = [];
 
     public function onEnable() : void {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getServer()->getCommandMap()->register("SimpleHub", new HubCommand($this));
         $this->getServer()->getCommandMap()->register("SimpleHub", new SetHubCommand($this));
         $this->getServer()->getCommandMap()->register("SimpleHub", new DeleteHubCommand($this));
@@ -28,6 +32,18 @@ class Main extends PluginBase implements Listener {
         $player = $event->getPlayer();
         $spawnLocation = $player->getWorld()->getSpawnLocation();
         $player->teleport($spawnLocation);
+    }
+
+    public function setHubLocation(string $worldName, Vector3 $location) {
+        $this->hubLocations[$worldName] = $location;
+    }
+
+    public function isHubLocationSet(string $worldName): bool {
+        return isset($this->hubLocations[$worldName]);
+    }
+
+    public function getHubLocation(string $worldName): ?Vector3 {
+        return $this->hubLocations[$worldName] ?? null;
     }
 
     public function getWorldManager(): WorldManager {
